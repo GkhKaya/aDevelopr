90af6a1a8a5f8ad57b6f9778f27932b6dcb0d521
GkhKaya <gkhkaya00@gmail.com>
2024-07-16 16:12:11 +0300

    Star rating added (swiftuı)

---
+++ SwiftVideos/SwiftVideos/SwiftVideos/StarRating.swift
@@ -0,0 +1,35 @@
+//
+//  StarRating.swift
+//  SwiftVideos
+//
+//  Created by Gokhan Kaya on 16.07.2024.
+//
+
+import SwiftUI
+
+struct StarRating: View {
+    @State private var rating = 0
+    var body: some View {
+        VStack{
+            Text("Puanınız: \(rating)")
+                .font(.headline)
+                .padding()
+            
+            HStack{
+                ForEach(1..<6){index in
+                    Image(systemName: index<=rating ? "star.fill": "star")
+                        .resizable()
+                        .frame(width: 30,height: 30)
+                        .foregroundStyle(.yellow)
+                        .onTapGesture {
+                            self.rating = index
+                        }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    StarRating()
+}
